970546c3855258f887130c744d85f9002203db7c
Tomisin <oyemadeoluwatomisin75@gmail.com>
2025-02-07 09:25:32 +0100

    app chores cleanimg

--- lib/code.ts
+++ lib/code.ts
@@ -1,35 +1,11 @@
 /// <reference types="@figma/plugin-typings" />
-
+import { formatDate } from '../src/helpers'
 
 // Get current user and format the date
 const currentUser = figma.currentUser;
 
-
 // Retrieve saved note from Figma's shared plugin data
 const savedNote = figma.root.getSharedPluginData("notePlugin", "documentNote");
-// const savedNoteToObj = JSON.parse(savedNote);
-// if (!savedNoteToObj.user) {
-//   figma.root.setSharedPluginData("notePlugin", "documentNote", "");
-// }
-
-const formatDate = (date: Date) => {
-  const months = [
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-  ];
-
-  const day = date.getDate();
-  const month = months[date.getMonth()];
-  const year = date.getFullYear();
-
-  let hours = date.getHours();
-  const minutes = date.getMinutes().toString().padStart(2, "0");
-  const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12 || 12; // Convert to 12-hour format
-
-  return `${day} ${month} ${year}, ${hours}:${minutes}${ampm}`;
-};
-
 const formattedDate = formatDate(new Date());
 
 // Load the plugin UI
---
+++ src/helpers.ts
@@ -0,0 +1,17 @@
+export const formatDate = (date: Date) => {
+  const months = [
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  let hours = date.getHours();
+  const minutes = date.getMinutes().toString().padStart(2, "0");
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12; // Convert to 12-hour format
+
+  return `${day} ${month} ${year}, ${hours}:${minutes}${ampm}`;
+};
